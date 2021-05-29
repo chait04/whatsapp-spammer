87777565c2b0f3b3ffbe4012a6f079043e682f4a
Chaitanya Khachane <ckhachane67@gmail.com>
2021-05-29 14:41:38 +0530

    fixed bugs

--- whatsapp.js
+++ whatsapp.js
@@ -1,57 +1,55 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 
-const app = async() => {
+const app = async () => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: false,
+      ignoreDefaultArgs: ['--enable-automation'],
+    });
+    const page = await browser.newPage();
+    await page._client.send('Emulation.clearDeviceMetricsOverride');
 
-    try {
-        const browser = await puppeteer.launch({ 
-            headless: false,
-            ignoreDefaultArgs: ["--enable-automation"]
-        })
-        const page = await browser.newPage();
-        await page._client.send("Emulation.clearDeviceMetricsOverride")
+    // setting user agent
+    await page.setUserAgent(
+      `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36`
+    );
 
-        // setting user agent
-        await page.setUserAgent(`Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36`)
+    await page.goto(`https://web.whatsapp.com/`);
 
-        await page.goto(`https://web.whatsapp.com/`)
+    // SEARCHES USER BY TITLE
+    await page.waitForSelector(`span[title="Your_contact"]`); //Enter you contac name here
+    await delay(5000);
 
-        // SEARCHES USER BY TITLE
-        await page.waitForSelector("div[data-tab='6']")
-        await delay(5000);
+    // selecting contact
+    const contactName = 'Contact_name'; //Contact Name
+    await page.click(`span[title='${contactName}']`);
 
-        // selecting contact
-        const contactName = 'Anjali Gupta Linkedin'
-        await page.click(`span[title='${contactName}']`)
-        // await page.waitForSelector('._1JAUF')
+    // focus on msgbar
+    const editor = await page.$(`div[data-tab="6"]`);
+    await editor.focus();
 
-        // focus on msgbar
-        const editor = await page.$("div[data-tab='6']")
-        await editor.focus()
+    // number of messages you want to send
+    const amountOfMsg = 100;
 
-
-        // no. of messages
-        const amountOfMsg = 5;
-
-        // automate msg
-        for(let i = 0; i < amountOfMsg; i++) {
-            await page.evaluate(() => {
-                const message = `testing automate message vro`
-                document.execCommand("InsertText", false, message)
-            })
-            await page.waitForSelector("span[data-testid='send']")
-            await page.click("span[data-testid='send']")
-            await delay(500)
-        }
-
-    } catch (error) {
-        console.log(error);
+    // automate msg
+    for (let i = 0; i < amountOfMsg; i++) {
+      await page.evaluate(() => {
+        //   write msg you want to send
+        const message = `Sample text to your contact`; //Enter message
+        document.execCommand('InsertText', false, message);
+      });
+      await page.waitForSelector("span[data-testid='send']");
+      await page.click("span[data-testid='send']");
+      await delay(500);
     }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+function delay(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 }
-
-function delay(time){
-    return new Promise(resolve => {
-        setTimeout(resolve, time)
-    })
-}
-app()
+app();
